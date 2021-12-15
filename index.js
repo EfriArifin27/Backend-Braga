d0f6cf1d13768cad3791694a47ee4345748f9d7a
Efri Triana Nur Arifin <efritriana27@gmail.com>
2021-12-15 08:47:33 +0700

    menambahkan fastify static,nodemon dan folder public

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
+const path = require('path')
+
+// Register Plugin
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, 'public'),
+  prefix: '/', // optional: default '/'
+})
 
 // Declare a route
-fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
+fastify.get('/', function (req, reply) {
+  reply.sendFile('index.html') // serving path.join(__dirname, 'public', 'myHtml.html') directly
 })
+
 
 // Run the server!
 const start = async () => {
